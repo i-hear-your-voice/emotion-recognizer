1ce683ae69bd04d1a326616279cc0ccdbd2a6e68
hyonzin <hyeonjin507@gmail.com>
2016-12-07 17:45:51 +0900

    Add comments

--- main.R
+++ main.R
@@ -2,20 +2,24 @@ library(tuneR)
 library(audio)
 library(base)
 
-path <- "/Users/hyeonjin/workspace/r/voice/speech_1206"
-
+# set path of voice files
+path <- "/Users/hyeonjin/workspace/r/emotion-recognizer/voice"
 setwd(path)
+
+# read the list of voice files
 wav.filename <- list.files()
+
+# get label (emotion)
 wav.split <- as.list(sapply(wav.filename, function(x) strsplit(x, "_") ))
 wav.emotion <- as.vector(sapply(wav.split, function(x) x[2]))
-wav.filename
-wav.emotion
 
-# wav.obj: mono wave files
+# get mono wav objects
 wav.obj <- sapply(wav.filename, function(x) mono(readWave(x), which = c("left", "right", "both")))
+
+# get mfcc data
 wav.mfcc <- sapply(wav.obj, function(x) melfcc(x))
 wav.mfcc.mean <- sapply(wav.mfcc, function(x) apply(x, 2, mean)) 
 
-wav.filename[1] # 1번째 오디오파일의 이름
-wav.emotion[1] # 1번째 오디오파일의 감정
-wav.mfcc.mean[, 1] # 1번째 오디오파일의 MFCC 1~12차수 (평균)
+## wav.filename[1]
+## wav.emotion[1] 
+## wav.mfcc.mean[, 1]
