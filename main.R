8707901810577d237607ab21175ce8f7d315665c
hyonzin <hyeonjin507@gmail.com>
2016-12-15 18:21:08 +0900

    SVM with Probability

--- main.R
+++ main.R
@@ -3,8 +3,13 @@ library(audio)
 library(base)
 library(e1071)
 
+normalize <- function(x) {
+  return((x-min(x)) / (max(x)-min(x)))
+}
+
 # set path of voice files
 path <- "/Users/hyeonjin/workspace/r/emotion-recognizer/voice"
+#path <- "/Users/hyeonjin/Downloads/rml/all_lang"
 setwd(path)
 
 # read the list of voice files
@@ -20,36 +25,22 @@ wav.obj <- sapply(wav.filename, function(x) mono(readWave(x), which = c("left", 
 # get mfcc data
 wav.mfcc <- sapply(wav.obj, function(x) melfcc(x))
 wav.mfcc.mean <- sapply(wav.mfcc, function(x) apply(x, 2, mean)) 
-
-wav.filename[1]
-wav.emotion[1] 
-wav.mfcc.mean[, 1]
-class(wav.mfcc.mean[, 1])
 wav.mfcc.mean <- data.frame(t(wav.mfcc.mean))
+wav.mfcc.mean <- as.data.frame(lapply(wav.mfcc.mean[,1:12], normalize))
 
-#
-wav.emotion
-wav.emotion.ang <- wav.emotion == "ang"
-wav.emotion.flat<- wav.emotion == "flat"
-wav.emotion.hap <- wav.emotion == "hap"
-wav.emotion.sad <- wav.emotion == "sad"
-wav.emotion.fear<- wav.emotion == "fear"
-wav.emotion.sup <- wav.emotion == "sup"
-
-wav.emotion.ang
-wav.emotion.flat
-wav.emotion.hap
-wav.emotion.sad
-wav.emotion.fear
-wav.emotion.sup
+#wav.emotion
+#wav.emotion.ang <- wav.emotion == "ang"
+#wav.emotion.flat<- wav.emotion == "flat"
+#wav.emotion.hap <- wav.emotion == "hap"
+#wav.emotion.sad <- wav.emotion == "sad"
+#wav.emotion.fear<- wav.emotion == "fear"
+#wav.emotion.sup <- wav.emotion == "sup"
 
 ind<-sample(2, length(wav.emotion), replace=T, prob=c(0.7, 0.3))
-ind
 train.data <- cbind(emotion=wav.emotion[ind==1], wav.mfcc.mean[ind==1,])
 test.data  <- cbind(emotion=wav.emotion[ind==2], wav.mfcc.mean[ind==2,])
-train.data
-test.data
-myFormula<-emotion~.
-wav.naiveBayes <-naiveBayes(myFormula, data = train.data)
-wav.naiveBayes.pred <- predict(wav.naiveBayes, test.data)
-table(Predictions= wav.naiveBayes.pred)
+formula <- emotion~.
+wav.svm <- svm(formula, data = train.data, probability = TRUE)
+wav.svm.pred <- predict(wav.svm, test.data, probability = TRUE)
+wav.svm.pred
+test.data[1]
